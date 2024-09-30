e9559eff7c89fd9a6bf618a7a3e122b9c4f67d23
Osama Soliman <solimanware@gmail.com>
2024-09-30 14:00:55 +0300

    go

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,7 @@
-function playAudio(file) {
-    var audio = new Audio(file);
-    audio.play();
-}
-
-const letters = "abcdefghijklmnopqrstuvwxyz".split("")
-buttonsElements = [];
-for (let letter of letters) {
-    const element = `<div onClick="playAudio('assets/letters/${letter}.mp3')" class="button"><h1>${letter.toUpperCase()}</h1></div>`
-    buttonsElements.push(element)
-}
-
-const element = `<div onClick="playAudio('assets/bravo.mp3')" class="button"><h1>Bravo</h1></div>`
-buttonsElements.push(element)
-
-var buttonsEl = document.getElementById("buttons");
-buttonsEl.innerHTML = buttonsElements.join(' ');
+const playAudio = file => new Audio(file).play();
+const createButton = (text, file) => `<div onClick="playAudio('${file}')" class="button"><h1>${text}</h1></div>`;
+const buttons = [
+    ...Array.from("abcdefghijklmnopqrstuvwxyz", letter => createButton(letter.toUpperCase(), `assets/letters/${letter}.mp3`)),
+    createButton("Bravo", "assets/bravo.mp3")
+];
+document.getElementById("buttons").innerHTML = buttons.join(' ');
